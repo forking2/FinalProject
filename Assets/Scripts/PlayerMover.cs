17cf2d47da747b4a51247b2ab77771ed00a5e974
forking2 <maranrovlik04@gmail.com>
2023-11-05 10:15:29 +0200

    +Трохи безкорисного коду?

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -2,18 +2,19 @@ using UnityEngine;
 
 public class PlayerMover : MonoBehaviour
 {
-    [SerializeField] private float _Jump;
-    public MeshRenderer MeshRenderer;
+    [SerializeField] private float _jump;
+    [SerializeField] private float _shift;
+    //public MeshRenderer MeshRenderer;
     private float GameTime;
     public static CursorLockMode lockState;
     public float maxJumpAcrossDistance;
-    public Rigidbody rb;
+    //public Rigidbody rb;
     public float gravityScale = 5;
     
 
     private void Update()
     {
-        Rigidbody.AddForce(Vector3 force);
+        //Rigidbody.AddForce(new Vector3(0f, 1f, 0f), force);
         if(Input.GetKey(KeyCode.W))
         {
             transform.Translate(0f, 0f, 0.01f);
@@ -30,20 +31,30 @@ public class PlayerMover : MonoBehaviour
         {
             transform.Translate(-0.01f, 0f, 0f);
         }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            transform.Translate(0f, _shift, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            transform.Translate(0f, -_shift, 0f);  
+        }
         if(Input.GetKeyDown(KeyCode.Space))
         {
+            transform.Translate(0f, _jump, 0f);
             
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyUp(KeyCode.Space))
         {
-              
+            transform.Translate(0f, -_jump, 0f);
         }
-    }
-    private void FixedUpdate()
-    {
         
-        rb.AddForce(Physics.gravity * (gravityScale - 1) * rb.mass);
-    }   
+    }
+    //private void FixedUpdate()
+    //{
+        
+    //    rb.AddForce(Physics.gravity * (gravityScale - 1) * rb.mass);
+    //}   
     public void OnGui()
     {
         if (GUI.Button(new Rect(0, 0, 100, 50), "Lock Cursor"))

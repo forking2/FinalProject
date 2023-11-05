c0382de332d3c54ad8846e4b621224c30425df44
forking2 <maranrovlik04@gmail.com>
2023-11-05 14:18:39 +0200

    Стрибок

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -4,62 +4,36 @@ public class PlayerMover : MonoBehaviour
 {
     [SerializeField] private float _jump;
     [SerializeField] private float _shift;
-    //public MeshRenderer MeshRenderer;
+    [SerializeField] public float _speed;
+    public Rigidbody rb;
     private float GameTime;
-    public static CursorLockMode lockState;
-    public float maxJumpAcrossDistance;
-    //public Rigidbody rb;
-    public float gravityScale = 5;
-    
+    public bool cubeIsOnTheGround = true;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
-        //Rigidbody.AddForce(new Vector3(0f, 1f, 0f), force);
-        if(Input.GetKey(KeyCode.W))
+        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * _speed;
+        float vertical = Input.GetAxis("Vertical") * Time.deltaTime * _speed;
+
+        transform.Translate(horizontal, 0f, vertical);
+        if(Input.GetKeyDown(KeyCode.Space) && cubeIsOnTheGround)
         {
-            transform.Translate(0f, 0f, 0.01f);
+            rb.AddForce(new Vector3(0f, _jump, 0f), ForceMode.Impulse);
+            cubeIsOnTheGround = false;
         }
-        if(Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(0f, 0f, -0.01f);
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(0.01f, 0f, 0f);
-        }
-        if(Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(-0.01f, 0f, 0f);
-        }
-        if(Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            transform.Translate(0f, _shift, 0f);
-        }
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            transform.Translate(0f, -_shift, 0f);  
-        }
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            transform.Translate(0f, _jump, 0f);
-            
-        }
-        if(Input.GetKeyUp(KeyCode.Space))
-        {
-            transform.Translate(0f, -_jump, 0f);
-        }
+
+        
+
         
     }
-    //private void FixedUpdate()
-    //{
-        
-    //    rb.AddForce(Physics.gravity * (gravityScale - 1) * rb.mass);
-    //}   
-    public void OnGui()
+    private void OnCollisionEnter(Collision collision)
     {
-        if (GUI.Button(new Rect(0, 0, 100, 50), "Lock Cursor"))
+        if(collision.gameObject.name == "Floor")
         {
-            Cursor.lockState = CursorLockMode.Locked;
+            cubeIsOnTheGround = true;
         }
-    }
+    }  
 }

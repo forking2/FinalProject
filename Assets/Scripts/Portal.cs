c7572175966ec59bfdf3d880ca5816bc00be24a6
forking2 <maranrovlik04@gmail.com>
2023-11-16 17:34:03 +0200

    perevirka

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -13,16 +13,16 @@ public class Portal : MonoBehaviour
     
     private void Update()
     {
-        // Position
+        
         Vector3 lookerPosition = Other.transform.worldToLocalMatrix.MultiplyPoint3x4(Camera.main.transform.position);
         lookerPosition = new Vector3(-lookerPosition.x, lookerPosition.y, -lookerPosition.z);
         PortalView.transform.localPosition = lookerPosition;
 
-        // Rotation
+        
         Quaternion difference = transform.rotation * Quaternion.Inverse(Other.transform.rotation * Quaternion.Euler(0,180,0));
         PortalView.transform.rotation = difference * Camera.main.transform.rotation;
 
-        // Clipping
+        
         PortalView.nearClipPlane = lookerPosition.magnitude;
     }
 }
--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -24,12 +24,12 @@ public class Teleporter : MonoBehaviour
 
     private void Teleport(Transform obj)
     {
-        // Position
+        
         Vector3 localPos = transform.worldToLocalMatrix.MultiplyPoint3x4(obj.position);
         localPos = new Vector3(-localPos.x, localPos.y, -localPos.z);
         obj.position = Other.transform.localToWorldMatrix.MultiplyPoint3x4(localPos);
 
-        // Rotation
+        
         Quaternion difference = Other.transform.rotation * Quaternion.Inverse(transform.rotation * Quaternion.Euler(0, 180, 0));
         obj.rotation = difference * obj.rotation;
     }
